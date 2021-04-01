048ddcdaa1fc05d7f5f0c96378f82f371b9ab5e7
Anthony Deplanque <anthony.deplanque3184@gmail.com>
2021-04-01 19:34:32 +0200

    Added some style for Characters
    Playing with <divs> and CSS

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -5,19 +5,24 @@ const Characters = (props) => {
     return (
         <div className="boxes-card">
             <div className="box-card">
-                <img className="image-card" src={props.image} />
+
                 <div className="name-card">
                     <h2>{props.name}</h2>
-                <div className = "bigblock-status"></div>
-                    <p className="status-gender">{props.gender}</p>
-                <div className="block-status">
-                    <p className="status-specie">Specie : {props.species}</p>
-                    <div>
-                    <div className={"status-card " + (props.status.charAt(0).toLowerCase() + props.status.slice(1)) + "-status"}></div>
-                    <p className="status-text">{props.status}</p>
-                    </div>
-                    <p className="status-location">{props.location.name}</p>
                 </div>
+
+                <div>
+                    <img className="image-card" src={props.image} />
+                </div>
+                <div className="bigblock-status">
+                    <div className="block-status">
+                        <p className="status-gender">{props.gender}</p>
+                        <p className="status-specie">Specie : {props.species}</p>
+                        <p className="status-location">{props.location.name}</p>
+                        <div>
+                            <p className="status-text">{props.status}</p>
+                            <div className={"status-card " + (props.status.charAt(0).toLowerCase() + props.status.slice(1)) + "-status"}></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
